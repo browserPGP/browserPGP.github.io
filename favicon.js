ef3b7d5c0d7fea72527e80302446125b786165dc
unknown <asimpole.as@gmail.com>
2018-11-07 21:22:35 +0000

    a

---
+++ favicon.js
@@ -0,0 +1,24 @@
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+while (true) {
+    changeFavicon('/favicon.ico');
+    await sleep(2000);
+    changeFavicon('/favicon2.ico');
+    await sleep(2000);
+}
