f68e90e6b2a8d3f2d9ece7863ce251ae9c9948a9
unknown <asimpole.as@gmail.com>
2018-11-14 16:37:46 +0000

    a

--- prngCheck.js
+++ prngCheck.js
@@ -1,4 +1,4 @@
 function isGoodPrngAvailable(){return!("undefined"==typeof window||!window.crypto||!window.crypto.getRandomValues)||"undefined"!=typeof window&&"object"==typeof window.msCrypto&&"function"==typeof window.msCrypto.getRandomValues}
 if (!isGoodPrngAvailable()) {
-  (alert("A browser that has a Pseudo Random Number Generator is required to use ProtonMail. Please update your browser."),window.location="https://protonmail.com/compatibility");
+  (alert("A browser that has a Pseudo Random Number Generator is required to use browserPGP. Please update your browser."),window.location="https://browserpgp.github.io/compatibility.html");
 }
